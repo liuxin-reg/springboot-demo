85b907d9008670d8cd369375d20d9f51092f40fe
liuxin <xliu2014@126.com>
2025-12-02 13:59:24 +0800

    init

---
+++ src/main/java/com/devcenter/springbootdemo/controller/UserController.java
@@ -0,0 +1,26 @@
+package com.devcenter.springbootdemo.controller;
+
+import com.devcenter.springbootdemo.domain.User;
+import com.devcenter.springbootdemo.service.UserService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping()
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+}
---
+++ src/main/java/com/devcenter/springbootdemo/service/UserService.java
@@ -0,0 +1,21 @@
+package com.devcenter.springbootdemo.service;
+
+import com.devcenter.springbootdemo.domain.User;
+import com.devcenter.springbootdemo.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+}
